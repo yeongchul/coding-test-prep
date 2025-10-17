f7807a426d7e96bfee785f955d2ede89813087de
김민정 <revan2015@naver.com>
2025-10-17 14:28:44 +0900

    solve: 백준 10828 - 스택

---
+++ baekjoon/src/BaekJoon10828.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BaekJoon10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=0; i<N; i++) {
+            String[] line = br.readLine().split(" ");
+
+            switch (line[0]) {
+                case "push":
+                    stack.push(Integer.parseInt(line[1]));
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "top":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+            }
+        }
+    }
+}
