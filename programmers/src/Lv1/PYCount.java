fe5397c08b15f356f5c32859d31d795503c94ed1
김민정 <revan2015@naver.com>
2025-10-18 14:25:22 +0900

    refactor: [PGS] 문자열 내 p와 y의 개수

--- programmers/src/Lv1/PYCount.java
+++ programmers/src/Lv1/PYCount.java
@@ -1,20 +1,18 @@
 package Lv1;
 
 public class PYCount {
-    class Solution {
-        boolean solution(String s) {
-            String[] c = s.split("");
+    boolean solution(String s) {
+        String[] c = s.split("");
 
-            int pCount = 0;
-            int yCount = 0;
+        int pCount = 0;
+        int yCount = 0;
 
-            for (String ch : c) {
-                if (ch.toLowerCase().equals("p")) pCount++;
-                if (ch.toLowerCase().equals("y")) yCount++;
-            }
-
-            if (pCount == yCount) return true;
-            return false;
+        for (String ch : c) {
+            if (ch.toLowerCase().equals("p")) pCount++;
+            if (ch.toLowerCase().equals("y")) yCount++;
         }
+
+        if (pCount == yCount) return true;
+        return false;
     }
 }
