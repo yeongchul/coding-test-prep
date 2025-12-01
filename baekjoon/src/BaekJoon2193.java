f74e9bc8818972ea7ea735f4832521be19454445
김민정 <revan2015@naver.com>
2025-12-01 21:25:05 +0900

    solve: [BOJ] 2193번: 이친수

---
+++ baekjoon/src/BaekJoon2193.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BaekJoon2193 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        long[] dp = new long[N+1];
+
+        if (N==1) {
+            System.out.println(1);
+            return;
+        } else if (N==2) {
+            System.out.println(1);
+            return;
+        }
+        
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i=3; i<=N; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
