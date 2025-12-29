c0491cb016e50352d8e9265a62d1297285e1100f
김민정 <revan2015@naver.com>
2025-12-29 17:45:40 +0900

    solve: [BOJ] 11726번: 2xn 타일링

---
+++ baekjoon/src/BaekJoon11726.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BaekJoon11726 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[n+2];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        if (n <= 2) {
+            System.out.println(dp[n]);
+            return;
+        }
+
+        for (int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
